29f1023f832acc21cbadc6a3aa42b6fe973025f8
Jérémy Renaut <jeremy.renaut1@gmail.com>
2023-01-25 22:46:19 +0100

    edit tables sql

--- sqlTables/table_Messages.sql
+++ sqlTables/table_Messages.sql
@@ -1,11 +1,12 @@
 CREATE TABLE `messages` (
 	`id` INT(11) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
-	`message` LONGTEXT NOT NULL COLLATE 'utf8mb3_general_ci',
+	`msg` TEXT NOT NULL COLLATE 'utf8mb3_general_ci',
 	`author` TEXT NOT NULL COLLATE 'utf8mb3_general_ci',
-	PRIMARY KEY (`id`) USING BTREE,
-	UNIQUE INDEX `UNIQUE` (`author`) USING HASH
+	`show` TINYINT(1) NOT NULL DEFAULT '1',
+	`createdAt` TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+	PRIMARY KEY (`id`) USING BTREE
 )
 COLLATE='utf8mb3_general_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=86
+AUTO_INCREMENT=175
 ;
